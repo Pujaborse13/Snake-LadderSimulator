a6d4ee2ccf8556794a2e4647c50eaabc20a58dd2
Puja Borse <pujaborse13@gmail.com>
2024-11-26 17:58:13 +0530

    Added to Master

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+public class SnakeAndLadder  {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the Snake and Ladder Game!");
+        int playerPosition = 0;
+        System.out.println("Starting Position of Player: " + playerPosition);
+
+
+    }
+}
