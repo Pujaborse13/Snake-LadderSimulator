4cb26f840478e1ceb4788aea82e0fee64d23ea5e
Puja Borse <pujaborse13@gmail.com>
2024-11-27 11:57:54 +0530

    UC3: Player check For the Option No Play,Snake,Ladder

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -10,5 +10,28 @@ public class SnakeAndLadder  {
         Random random = new Random();
         int dieRoll = random.nextInt(6) + 1;
         System.out.println("Player rolls the die and gets: " + dieRoll);
+
+        int option = random.nextInt(3);
+        switch (option) {
+            case 0: // No Play
+                System.out.println("Option: No Play. Player stays at the same position.");
+                break;
+            case 1: // Ladder
+                System.out.println("Option: Ladder! Player moves ahead by " + dieRoll + " positions.");
+                playerPosition += dieRoll;
+                break;
+
+            case 2: // Snake
+                System.out.println("Option: Snake! Player moves behind by " + dieRoll + " positions.");
+                playerPosition -= dieRoll;
+
+                if (playerPosition < 0) {
+                    playerPosition = 0;
+                }
+                break;
+        }
+
+        System.out.println("Player's current position: " + playerPosition);
+        }
     }
-}
+
