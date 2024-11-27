6d615e561616c19f17b01a67e244531957baba6d
Puja Borse <pujaborse13@gmail.com>
2024-11-27 11:38:48 +0530

    UC2: Player rolls the die

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,8 +1,14 @@
+import java.util.Random;
+
 public class SnakeAndLadder  {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
 
         int playerPosition = 0;
         System.out.println("Starting Position of Player: " + playerPosition);
+
+        Random random = new Random();
+        int dieRoll = random.nextInt(6) + 1;
+        System.out.println("Player rolls the die and gets: " + dieRoll);
     }
 }
