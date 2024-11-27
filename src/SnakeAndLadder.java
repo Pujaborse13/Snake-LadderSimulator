c3fed04729f45564c06b386217e805ba746dce65
Puja Borse <pujaborse13@gmail.com>
2024-11-27 10:56:12 +0530

    UC1: Snake and Ladder game starts with a single player

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,10 +1,8 @@
 public class SnakeAndLadder  {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
+
         int playerPosition = 0;
         System.out.println("Starting Position of Player: " + playerPosition);
-
-
     }
 }
